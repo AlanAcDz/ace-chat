d2df26d9a8b6a26768098433e43fe2723270d196
Alan Acuña <alantheace@outlook.com>
2025-06-10 18:44:54 -0600

    feat: add new UI components and update dependencies

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+	import { Toaster } from '$lib/components/ui/sonner/index.js';
+
 	import '../app.css';
 
 	let { children } = $props();
 </script>
 
 {@render children()}
+<Toaster />
